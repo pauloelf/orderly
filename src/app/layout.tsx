9a3ef0bdf7634d21d8cb88ce574621d3e247ab3a
Paulo Sergio <contato.paulofws@gmail.com>
2025-07-19 18:21:25 -0300

    feat: dark mode set as default

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
+    <html className="dark" lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
