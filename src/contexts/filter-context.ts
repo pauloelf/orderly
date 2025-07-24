5774e7c99f951cf16a88e60b9ecf985d454dc94b
Paulo Sergio <contato.paulofws@gmail.com>
2025-07-24 20:11:54 -0300

    feat: filter-context added

---
+++ src/contexts/filter-context.ts
@@ -0,0 +1,21 @@
+import { createContext } from 'react'
+import type { FiltersProps } from '@/@types'
+
+type FilterContextType = {
+  filters: FiltersProps
+  setFilters: (newFilters: Partial<FiltersProps>) => void
+}
+
+export const DEFAULT_FILTERS: FiltersProps = {
+  page: 1,
+  sort: 'created_at',
+  status: '',
+  search: '',
+}
+
+const DEFAULT_VALUES: FilterContextType = {
+  filters: DEFAULT_FILTERS,
+  setFilters: () => null,
+}
+
+export const FilterContext = createContext<FilterContextType>(DEFAULT_VALUES)
