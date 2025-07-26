c8a8008b34a819c93b4a704be473b6550c166d95
Paulo Sergio <contato.paulofws@gmail.com>
2025-07-25 21:56:07 -0300

    feat: search added

---
+++ src/components/orders/search-input.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { Search } from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { useDebounce } from 'use-debounce'
+import { Input } from '@/components/ui/input'
+import { useFilters } from '@/hooks/useFilters'
+
+export function SearchInput() {
+  const { setFilters, filters } = useFilters()
+  const [search, setSearch] = useState(filters.search)
+
+  const [debouncedSearch] = useDebounce(search, 500)
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: explanation
+  useEffect(() => {
+    setFilters({ search: debouncedSearch, page: 1, status: filters.status })
+  }, [debouncedSearch])
+
+  return (
+    <div className="relative">
+      <Search className="absolute top-2.5 left-2.5 h-4 w-4 text-muted-foreground" />
+      <Input
+        className="w-full pl-8 md:w-[200px] lg:w-[360px]"
+        onChange={({ target }) => setSearch(target.value)}
+        placeholder="Busque por nome..."
+        type="search"
+        value={search}
+      />
+    </div>
+  )
+}
