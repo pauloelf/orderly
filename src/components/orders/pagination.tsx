e99010db2129e1e492e81c2b779d13a40b9fe672
Paulo Sergio <contato.paulofws@gmail.com>
2025-07-27 21:54:32 -0300

    feat: pagination added

---
+++ src/components/orders/pagination.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import type { MetaProps } from '@/@types'
+import {
+  Pagination as PaginationComponent,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination'
+import { useFilters } from '@/hooks/useFilters'
+
+interface IMeta {
+  meta: MetaProps | undefined
+}
+
+export function Pagination({ meta }: IMeta) {
+  const { setFilters, filters } = useFilters()
+
+  const { links, last_page, current_page } = meta ?? {}
+  const linksWithId = links?.map((link, index) => ({ ...link, id: index }))
+
+  const isFirstPage = current_page === 1
+  const isLastPage = current_page === last_page
+
+  const handleChangePage = (page: number, isDisabled: boolean) => {
+    if (!isDisabled) {
+      setFilters({ page, search: filters.search, status: filters.status })
+    }
+  }
+
+  return (
+    <PaginationComponent>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious
+            className={isFirstPage ? 'cursor-not-allowed' : 'cursor-pointer'}
+            isActive={!isFirstPage}
+            onClick={() =>
+              handleChangePage(Number(current_page) - 1, isFirstPage)
+            }
+          />
+        </PaginationItem>
+        {linksWithId?.map((link) => {
+          if (
+            link.label.includes('Anterior') ||
+            link.label.includes('Próximo')
+          ) {
+            return null
+          }
+          if (link.label.includes('...')) {
+            return (
+              <PaginationItem key={link.id}>
+                <PaginationEllipsis />
+              </PaginationItem>
+            )
+          }
+
+          return (
+            <PaginationItem className="hidden md:inline-flex" key={link.id}>
+              <PaginationLink
+                className="cursor-pointer"
+                isActive={link.active}
+                onClick={() =>
+                  handleChangePage(Number(link.label), link.active)
+                }
+              >
+                {link.label}
+              </PaginationLink>
+            </PaginationItem>
+          )
+        })}
+        <PaginationItem>
+          <PaginationNext
+            className={isLastPage ? 'cursor-not-allowed' : 'cursor-pointer'}
+            isActive={!isLastPage}
+            onClick={() =>
+              handleChangePage(Number(current_page) + 1, isLastPage)
+            }
+          />
+        </PaginationItem>
+      </PaginationContent>
+    </PaginationComponent>
+  )
+}
